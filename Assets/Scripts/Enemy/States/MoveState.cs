e3d82f450028d6df19c19929305fd0f0eca4aa79
YuraSkydan <87515048+YuraSkydan@users.noreply.github.com>
2021-08-12 06:16:02 +0300

    Expanded enemy class, added new enemy

---
+++ Assets/Scripts/Enemy/States/MoveState.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+public class MoveState : State
+{
+    public MoveState(EnemyAI enemy, StateMachine stateMachine) : base(enemy, stateMachine) { }
+
+    public override void Enter()
+    {
+        enemy.PlayAnimation("Run");
+    }
+
+    public override void PhysicsUpdate()
+    {
+        if (enemy.IsDetected() || enemy._Follow())
+        {
+            enemy.LookAtPlayer();
+
+            if (enemy.CanAttack())
+                enemy.SetState(EnemyAI.States.Attack);
+            else
+                enemy.Move();
+        }
+        else
+        {
+            enemy.SetState(EnemyAI.States.Idle);
+        }    
+    }
+
+    public override void Exit()
+    {
+        enemy.StopMoving();
+    }
+}
